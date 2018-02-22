1ae9e3f34472321a137da7f1b238e34f740c9334
Joost Yervante Damad <joost@damad.be>
2018-02-22 11:00:30 +0100

    introduce EntryBuilder as well

--- src/lib.rs
+++ src/lib.rs
@@ -105,15 +105,80 @@ impl FeedBuilder {
 }
 
 /// a feed `Entry`
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Entry {
-    title:String,
-    url:String,
-    content:String,
-    date:DateTime<Local>,
+    title:Option<String>,
+    url:Option<String>,
+    content:Option<String>,
+    date:Option<DateTime<Local>>,
     date_updated:Option<DateTime<Local>>,
     author:Option<String>,
     email:Option<String>,
+}
+
+/// a builder for `Feed`
+#[derive(Debug, Default)]
+pub struct EntryBuilder(Entry);
+
+impl EntryBuilder {
+    /// set the title of the entry
+    pub fn title<T:Into<String>>(mut self, title:T) -> Self {
+        self.0.title = Some(title.into());
+        self
+    }
+    
+    /// set the url of the entry
+    pub fn url<T:Into<String>>(mut self, url:T) -> Self {
+        self.0.url = Some(url.into());
+        self
+    }
+    
+    /// set the content of the entry
+    pub fn content<T:Into<String>>(mut self, content:T) -> Self {
+        self.0.content = Some(content.into());
+        self
+    }
+    
+    /// set the date of the entry
+    pub fn date(mut self, date:DateTime<Local>) -> Self {
+        self.0.date = Some(date);
+        self
+    }
+    
+    /// set the author of the entry
+    pub fn author<T:Into<String>>(mut self, author:T) -> Self {
+        self.0.author = Some(author.into());
+        self
+    }
+    
+    /// set the email of the entry
+    pub fn email<T:Into<String>>(mut self, email:T) -> Self {
+        self.0.email = Some(email.into());
+        self
+    }
+    
+    /// set the date updated of the entry
+    pub fn date_updated(mut self, date_updated:DateTime<Local>) -> Self {
+        self.0.date_updated = Some(date_updated);
+        self
+    }
+    
+    /// build the `EntryBuilder` in a `Entry`
+    pub fn build(self) -> Result<Entry, Error> {
+        if self.0.title.is_none() {
+            return Err("Entry title is mandatory".into())
+        }
+        if self.0.url.is_none() {
+            return Err("Entry url is mandatory".into())
+        }
+        if self.0.content.is_none() {
+            return Err("Entry content is mandatory".into())
+        }
+        if self.0.date.is_none() {
+            return Err("Entry date is mandatory".into())
+        }
+        Ok(self.0)
+    }
 }
 
 fn make_atom_feed(feed:Feed,entries:Vec<atom_syndication::Entry>) -> Result<atom_syndication::Feed, Error> {
@@ -134,7 +199,7 @@ fn make_atom_feed(feed:Feed,entries:Vec<atom_syndication::Entry>) -> Result<atom
 }
 
 fn make_atom_entry(feed:&Feed, entry:Entry) -> Result<atom_syndication::Entry, Error> {
-    let published_date = entry.date.to_rfc3339();
+    let published_date = entry.date.unwrap().to_rfc3339();
     let updated_date = entry.date_updated.map(|x| x.to_rfc3339()).unwrap_or(published_date.clone());
     
     let mut content = atom_syndication::Content::default();
@@ -148,8 +213,8 @@ fn make_atom_entry(feed:&Feed, entry:Entry) -> Result<atom_syndication::Entry, E
         .build()?;
     
     let entry = atom_syndication::EntryBuilder::default()
-        .title(entry.title)
-        .id(entry.url)
+        .title(entry.title.unwrap())
+        .id(entry.url.unwrap())
         .published(published_date)
         .updated(updated_date)
         .authors(vec![person])
